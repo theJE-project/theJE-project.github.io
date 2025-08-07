add6ad6d89a7a26d15239eeaadd9b4985d7b926a
Soda <k.sua923@gmail.com>
2025-08-07 16:45:46 +0900

    안쓰는거 주석함

--- src/pages/home/loader.jsx
+++ src/pages/home/loader.jsx
@@ -1,22 +1,21 @@
 import { springBoot } from "@axios";
 
-
 const user = localStorage.getItem('user-id');
-const getCommunities = async () => {
-    try {
-        const response = await springBoot.get('/communities', {
-            params: {
-                category: 1,
-            }
-        });
-        const result = response.data;
-        console.log("피드 불러오기 성공:", result);
-        return result;
-    } catch (error) {
-        console.error("피드 불러오기 실패:", error);
-        return [];
-    }
-}
+// const getCommunities = async () => {
+//     try {
+//         const response = await springBoot.get('/communities', {
+//             params: {
+//                 category: 1,
+//             }
+//         });
+//         const result = response.data;
+//         console.log("피드 불러오기 성공:", result);
+//         return result;
+//     } catch (error) {
+//         console.error("피드 불러오기 실패:", error);
+//         return [];
+//     }
+// }
 
 // 팔로우 여부 조회하려고 만든 거
 const getCommunities1 = async () => {
@@ -28,7 +27,7 @@ const getCommunities1 = async () => {
             }
         });
         const result = response.data;
-        console.log("피드 불러오기 성공:", result);
+        console.log("피드 불러오기 성공(isFollowing 포함)", result);
         return result;
     } catch (error) {
         console.error("피드 불러오기 실패:", error);
@@ -73,11 +72,11 @@ const getFollowingCommunities = async () => {
 
 
 export const loader = async ({ params, request }) => {
-    const communities = await getCommunities();
+    // const communities = await getCommunities();
     const communities1 = await getCommunities1();
     const followingCommunities = await getFollowingCommunities();
     return {
-        communities: communities,
+        // communities: communities,
         communities1: communities1,
         followingCommunities: followingCommunities,
     }
